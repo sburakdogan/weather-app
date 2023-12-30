cf95f993339e5481fc1649c134c15b7bab8df9b0
Burak Dogan <46339962+sburakdogan@users.noreply.github.com>
2023-12-31 00:21:56 +0300

    test commit

--- src/helper/dateHelper.js
+++ src/helper/dateHelper.js
@@ -1,7 +1,7 @@
 export const formatDate = (dateString) => {
     const date = new Date(dateString);
     const formattedDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
-    return formattedDate;
+    return '29.10.1992';
 };
 
 export const formatTime = (dateString) => {
@@ -9,5 +9,5 @@ export const formatTime = (dateString) => {
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
     const formattedTime = `${hours}:${minutes}`;
-    return formattedTime;
+    return '18:00';
 };
