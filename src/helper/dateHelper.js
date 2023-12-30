2715f14b15c03f4fd99e5fa23a4e940548839aac
Burak Dogan <46339962+sburakdogan@users.noreply.github.com>
2023-12-31 00:30:07 +0300

    fixed format date helpers

--- src/helper/dateHelper.js
+++ src/helper/dateHelper.js
@@ -1,13 +1,9 @@
 export const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const formattedDate = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
-    return '29.10.1992';
+    return dateString.split(' ')[0].split('-').reverse().join('.');
 };
 
 export const formatTime = (dateString) => {
-    const date = new Date(dateString);
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    const formattedTime = `${hours}:${minutes}`;
-    return '18:00';
+    const hour = dateString.split(' ')[1].split(':');
+    hour.pop();
+    return hour.join(':');
 };
